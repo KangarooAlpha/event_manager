a90052a33e04d72ad3ec8fc851b5d0a8022d60f0
Omar <omar.fayyaz@ryerson.ca>
2024-09-12 19:16:46 -0400

    finished time targeting assignment

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,8 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'date'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -48,6 +50,13 @@ def clean_phone_numbers(number)
   number
 end
 
+def write_date_and_time(name, date, time)
+  filename = "lib/d_reg.txt" #unless File.exist?('lib/d_reg.txt')
+  File.open(filename, 'a') do |file| 
+    file.puts "#{name} registered on #{date} at #{time}\n"
+  end
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -59,14 +68,24 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+if File.exist?('lib/d_reg.txt') then File.delete('lib/d_reg.txt') end
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
   number = clean_phone_numbers(row[:homephone])
+  dt = (row[:regdate]).split(" ")
+  d = dt[0].split("/")
+  d[2] = (d[2].to_i + 2000).to_s
+  d = d.join("-")
+  d = Date.strptime(d, '%m-%d-%Y')
+  t = (Time.parse(dt[1])).strftime("%k:%M")
+
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id,form_letter)
+  write_date_and_time(name,d,t)
 end
